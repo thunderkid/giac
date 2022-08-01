ccfebd93dd52cd017c80dc38dd707ab66ca83893
thunderkid <thunderkid@smalliq.com>
2022-08-01 22:03:01 +0000

    Added task so ./gradlew createGiacWasmJsForNode followed by node nodetoy.js runs giac wasm via node.

---
+++ src/giac.js/nodetoy.js
@@ -0,0 +1,23 @@
+//var factory = require('file://./giacggb.js');
+var factory = require('./giac.node.wasm.js')
+//var factory = require('./giac.wasm2.js');
+
+factory().then((instance) => {
+  const caseval = instance.cwrap('caseval', 'string', ['string']);
+  console.log(caseval("simplify(22x-33x)"));
+  console.log(caseval("factor(x^2-1)"));
+  console.log(caseval("cfactor(x^4+4)"));
+  console.log(caseval("cfactor(x^4+81)"));
+  console.log(caseval("simplify(x^4+81-81)"));
+  const start = Date.now();
+    for (let i = 0; i < 1000; i++) {
+        caseval("cfactor(x^4+81)");
+        //caseval("simplify(x^4+81-81)");
+    }
+    const end = Date.now();
+
+    console.log('took ');
+    console.log(end-start);
+
+
+});

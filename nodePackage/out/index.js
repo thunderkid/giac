36ddfbde9970684b7eb1e256774fbee3f635126b
thunderkid <thunderkid@smalliq.com>
2022-08-07 16:48:59 -0700

    Wrapped giac initialize and caseval more tidily.

--- nodePackage/out/index.js
+++ nodePackage/out/index.js
@@ -1,1 +1,20 @@
+const factory = require('./giac.node.wasm');
+
+let caseval = null;
+
+export function initialize() {
+    return factory().then((theInstance) => {
+        caseval = theInstance.cwrap('caseval', 'string', ['string'])
+    });
+}
+
+export function runEval(str) {
+    if (!caseval)
+        throw Error(`Giac not initialized`);
+    return caseval(str);
+}
+
+
+
+
 export * from './parsley';
